ff097a7f54ddac368efe53162f5fe1a37aa33036
Josh Clark <toastyghost@live.com>
2014-06-22 21:46:35 +0100

    added plugin file

---
+++ biglinkwidget.php
@@ -0,0 +1,63 @@
+<?php
+/*
+Plugin Name: Big Link Widget
+Description: Puts a direct link to a provided URL in the sidebar as a heading.
+Author: Josh Clark
+Version: 1
+Author URI: http://khameleon.org/work
+*/
+
+class BigLinkWidget extends WP_Widget {
+	function BigLinkWidget() {
+		$widget_ops = array(
+			'classname' => 'BigLinkWidget',
+			'description' => 'Displays a link in the sidebar as a heading.'
+		);
+		
+		$this->WP_Widget('BigLinkWidget', 'Sidebar heading link', $widget_ops);
+	}
+	
+	function form($instance) {
+		$instance = wp_parse_args( (array) $instance, array('title' => '', 'link' => ''));
+		$title = $instance['title'];
+		$link = $instance['link']
+		?>
+		
+		<p><label for="<?php echo $this->get_field_id('title'); ?>">Title: <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo attribute_escape($title); ?>" /></label></p>
+		<p><label for="<?php echo $this->get_field_id('link'); ?>">Link: <input class="widefat" id="<?php echo $this->get_field_id('link'); ?>" name="<?php echo $this->get_field_name('link'); ?>" type="text" value="<?php echo attribute_escape($link); ?>" /></label></p>
+		
+		<?php
+	}
+	
+	function update($new_instance, $old_instance) {
+		$instance = $old_instance;
+		$instance['title'] = $new_instance['title'];
+		$instance['link'] = $new_instance['link'];
+		
+		return $instance;
+	}
+
+	function widget($args, $instance) {
+		extract($args, EXTR_SKIP);
+		
+		echo $before_widget;
+		
+		$title = empty($instance['title']) ? ' ' : apply_filters('widget_title', $instance['title']);
+		
+		if (!empty($link)) {
+			echo '<a href="', $link, '">';
+			
+			if (!empty($title)) {
+				echo $before_title, $title, $after_title;
+			}
+			
+			echo '</a>';
+		}
+		
+		echo $after_widget;
+	}
+}
+
+add_action('widgets_init', create_function('', 'return register_widget("BigLinkWidget");'));
+
+?>
